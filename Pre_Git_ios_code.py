e915dc16e7ef128310a7aff72e7a1e2f951f284e
Jamie Broen <jamie.w.brown97@gmail.com>
2019-02-27 15:48:20 +0000

    Adding the file with the if statement to the development branch

--- Pre_Git_ios_code.py
+++ Pre_Git_ios_code.py
@@ -11,11 +11,11 @@ from smtplib import SMTPException
 import platform
 
 sys = platform.system()
+#Filename
+file = input("What file would you like to send?")+ ".dat"
 
 if sys == ("Darwin"):
-	
-	file = input("What file would you like to send?")+ ".dat"
-
+	# Data capture ios #
 	console.alert('Motion Plot', 'When you tap Continue, accelerometer (motion) data will be recorded for 5 seconds.', 'Continue')
 	motion.start_updates()
 	sleep(0.2)
@@ -23,9 +23,9 @@ if sys == ("Darwin"):
 	num_samples = 100
 	data = []
 	for i in range(num_samples):
-		sleep(0.10)
-	g = motion.get_gravity()
-	data.append(g)
+                sleep(0.10)
+                g = motion.get_gravity()
+                data.append(g)
 	motion.stop_updates()
 	print('Capture finished, plotting...')
 
@@ -36,37 +36,86 @@ if sys == ("Darwin"):
 	pickle_out.close()
 	
 	print("Finished")
-########
-	msg = MIMEMultipart()
-	msg['From']= input("what's your email?")
-	msg['To'] = input("Whom do you wish to email?")
-	msg['Subject'] = "You got mail!"
-	msg.preamble = "TEST"
-	password = input("what is your password?")
+else:
+        # Data capture android #
+        import androidhelper
+        import time
+        from datetime import datetime
+        import pickle 
+        import os
+        os.chdir('/storage/emulated/0/acc.data/data_recorded')
+# Initialise
+        droid = androidhelper.Android()
+        dt = 100  # Sets min. time between measurements
+        X = []
+        Y = []
+        Z = []
+        t = []
 
+# Record acceleration data (2 = accel)
+        droid.startSensingTimed(2, dt)  # Selects sensor
+        print('**** Recording ****')
+        count = 0
+        N = 100
+        for i in range(0 , N):
+    # Read x, y and z accel.
+            x = droid.sensorsReadAccelerometer().result[0]
+            y = droid.sensorsReadAccelerometer().result[1]
+            z = droid.sensorsReadAccelerometer().result[2]   
+        
+    # Store accelerations and time
+            X.append(x)
+            Y.append(y)
+            Z.append(z)
+            t.append(datetime.now().strftime("%H:%M:%S.%f"))
+            
+    # Sometimes print time and iteration 
+            count += 1
+            if count == 10:
+                print('Iteration', (i+1), 'of', N)
+                print('Time:', datetime.now().strftime("%H:%M:%S.%f"))
+                count = 0
+    
+    # Pause before taking next measurement
+            time.sleep(dt/1000.0) 
 
-	part = MIMEBase('application', "ocetot-stream")
-#trying shit out
-	part.set_payload(open(file, "rb").read())
-	encoders.encode_base64(part)
-#trying shit out
-	part.add_header('Content-Disposition', 'attachment', filename = file)
-	msg.attach(part)
+# Stop recording    
+        droid.stopSensing()
+        print('**** Finished ****')
 
-	body = input("what do you wish to write to them?")
-	msg.attach(MIMEText(body,'html'))
+# Save time history data
+        file = open('data.data','wb')
+        my_tuple = tuple((X, Y, Z, t))
+        pickle.dump(my_tuple, file)
+        
+######## Emailing #########
+        
+msg = MIMEMultipart()
+msg['From']= input("what's your email?")
+msg['To'] = input("Whom do you wish to email?")
+msg['Subject'] = "You got mail!"
+msg.preamble = "TEST"
+password = input("what is your password?")
+part = MIMEBase('application', "ocetot-stream")
+part.set_payload(open(file, "rb").read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment', filename = file)
+msg.attach(part)
+
+body = input("what do you wish to write to them?")
+msg.attach(MIMEText(body,'html'))
 
     
-	try:
-    		s = smtplib.SMTP('smtp.gmail.com', 587)
-    		s.ehlo()
-    		s.starttls()
-    		s.ehlo()
-    		s.login(user = msg['From'], password = password)
-    		s.sendmail(msg['From'], msg['To'], msg.as_string())
-    		s.quit()
-	except SMTPException as error:
-        		print ("ERROR")
+try:
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(user = msg['From'], password = password)
+        s.sendmail(msg['From'], msg['To'], msg.as_string())
+        s.quit()
+except SMTPException as error:
+       		print ("ERROR")
 
 
 
